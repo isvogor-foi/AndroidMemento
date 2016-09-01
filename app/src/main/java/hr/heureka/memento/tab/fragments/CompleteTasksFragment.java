9224e3adc704d0601fd1c7ed772b4aba848a7744
isvogor-foi <isvogor@foi.hr>
2016-09-01 14:34:49 +0200

    03 Dorađen fragment za prikaz završenih aktivnosti

--- app/src/main/java/hr/heureka/memento/tab/fragments/CompleteTasksFragment.java
+++ app/src/main/java/hr/heureka/memento/tab/fragments/CompleteTasksFragment.java
@@ -2,12 +2,17 @@ package hr.heureka.memento.tab.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import hr.heureka.memento.R;
+import hr.heureka.memento.adapters.ActiveTasksRecycleViewAdapter;
+import hr.heureka.memento.entities.DbTask;
 
 
 /**
@@ -15,11 +20,27 @@ import hr.heureka.memento.R;
  */
 public class CompleteTasksFragment extends Fragment {
 
+    @Bind(R.id.rv_completed_tasks)
+    RecyclerView recycleView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_complete_tasks, container, false);
         ButterKnife.bind(this, rootView);
         return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recycleView.setHasFixedSize(true);
+
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        recycleView.setLayoutManager(llm);
+
+        recycleView.setAdapter(new ActiveTasksRecycleViewAdapter(DbTask.getAll(1), getContext()));
+
     }
 
 }
