2dc5dd0e8a57cd19dedefad14e8255d1f7d700f5
isvogor-foi <isvogor@foi.hr>
2016-09-01 13:39:13 +0200

    00 Dodana infrastruktura za rad sa tabovima (adapteri)

---
+++ app/src/main/java/hr/heureka/memento/adapters/ActiveTasksRecycleViewAdapter.java
@@ -0,0 +1,9 @@
+package hr.heureka.memento.adapters;
+
+/**
+ * Created by ivan on 5.7.2016..
+ */
+public class ActiveTasksRecycleViewAdapter{
+
+
+}
---
+++ app/src/main/java/hr/heureka/memento/viewholders/TaskViewHolder.java
@@ -0,0 +1,11 @@
+package hr.heureka.memento.viewholders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by ivan on 1.9.2016..
+ */
+public class TaskViewHolder extends RecyclerView.ViewHolder {
+    public TaskViewHolder(View itemView) { super(itemView); }
+}
